9bb4d29e203f9691eff718d7fd56242b7e5df9ce
Nico Rithner <nico@altvia.com>
2021-07-22 08:55:57 -0600

    Complete 'Composing Components' lesson
    Co-authored-by: Nico nicorithner@gmail.com

--- app.js
+++ app.js
@@ -7,7 +7,40 @@ const Header = () => {
   );
 };
 
+const Player = () => {
+  return (
+    <div className="player">
+      <span className="player-name">
+        Guil
+      </span>
+
+      <Counter />
+    </div>
+  );
+}
+
+const Counter = () => {
+  return (
+    <div className="counter">
+      <button className="counter-action decrement"> - </button>
+      <span className="counter-score">35</span>
+      <button className="counter-action increment"> + </button>
+    </div>
+  );
+}
+
+const App = () => {
+  return (
+    <div className="scoreboard">
+      <Header />
+
+      {/* players list */}
+      <Player />
+    </div>
+  );
+}
+
 ReactDOM.render(
-  <Header />,
+  <App />,
   document.getElementById('root')
 );
