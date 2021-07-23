725d6aeb7d973fda9021148c1495f4fb7b657e6a
Nico Rithner <nico@altvia.com>
2021-07-23 11:42:52 -0600

    Add constructor to class Counter
    Co-authored-by: Nico nicorithner@gmail.com

--- app.js
+++ app.js
@@ -43,11 +43,18 @@ const Player = (props) => {
 }
 
 class Counter extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      score: 0
+    };
+  }
+
   render() {
     return (
       <div className="counter">
         <button className="counter-action decrement"> - </button>
-        <span className="counter-score">{ this.props.score }</span>
+        <span className="counter-score">{ this.state.score }</span>
         <button className="counter-action increment"> + </button>
       </div>
     );
@@ -64,7 +71,6 @@ const App = (props) => {
         <Player
           key={player.id.toString()}
           name={player.name}
-          score={player.score}
         />
       )}
     </div>
